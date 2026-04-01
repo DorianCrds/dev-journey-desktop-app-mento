06a7e6ff969e58dd6fe5971c414d63995ebee0c6
Dorian CARDOSO <doriancardoso00@gmail.com>
2026-04-01 17:56:02 +0200

    style(Dashboard): applies new style to dashboard view

--- app/presenters/dashboard_presenter.py
+++ app/presenters/dashboard_presenter.py
@@ -9,9 +9,12 @@ from PySide6.QtCharts import (
     QValueAxis,
 )
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QBrush
 from PySide6.QtWidgets import QLabel, QSizePolicy
 
 from app.services.dashboard_service import DashboardService
+from app.ui.theme.colors import Colors
+from app.ui.theme.typography import Typography
 from app.ui.views.pages.dashboard.dashboard_view import DashboardView
 from app.ui.views.pages.dashboard.stat_card import StatCard
 
@@ -67,15 +70,30 @@ class DashboardPresenter:
         pie_series.append("Acquired", global_stats.acquired)
         pie_series.append("To learn", global_stats.to_learn)
 
+        pie_series.slices()[0].setBrush(QBrush(Colors.STATUS_ACQUIRED_CHART_BG))
+        pie_series.slices()[0].setLabelColor(Qt.GlobalColor.black)
+
+        pie_series.slices()[1].setBrush(QBrush(Colors.STATUS_TOLEARN_CHART_BG))
+
+        def on_hovered(slice, state):
+            slice.setExploded(state)
+
+        pie_series.hovered.connect(on_hovered)
+
         pie_chart = QChart()
         pie_chart.addSeries(pie_series)
+
         pie_chart.setTitle("Overall distribution")
+        pie_chart.setTitleFont(Typography.get_font(Typography.TEXT_NORMAL, Typography.MEDIUM))
+
         pie_chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
 
-        pie_chart_view = QChartView(pie_chart)
-        pie_chart_view.setMinimumHeight(300)
+        pie_chart.legend().setFont(Typography.get_font(Typography.META, Typography.REGULAR))
 
-        pie_chart_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        pie_chart_view = QChartView(pie_chart)
+        pie_chart_view.setMinimumHeight(400)
+
+        pie_chart_view.setRenderHint(pie_chart_view.renderHints())
 
         self._view.dashboard_charts_h_layout.addWidget(pie_chart_view, 1)
 
@@ -92,12 +110,17 @@ class DashboardPresenter:
         bar_set = QBarSet("Progression %")
         bar_set.append(values)
 
+        bar_set.setColor(Colors.STATUS_ACQUIRED_CHART_BG)
+
         bar_series = QBarSeries()
         bar_series.append(bar_set)
 
         bar_chart = QChart()
         bar_chart.addSeries(bar_series)
         bar_chart.setTitle("Progression by categories")
+        bar_chart.setTitleFont(Typography.get_font(Typography.TEXT_NORMAL, Typography.MEDIUM))
+        bar_chart.legend().setFont(Typography.get_font(Typography.META, Typography.REGULAR))
+
         bar_chart.setAnimationOptions(QChart.AnimationOption.SeriesAnimations)
 
         axis_x = QBarCategoryAxis()
@@ -106,6 +129,20 @@ class DashboardPresenter:
         axis_y = QValueAxis()
         axis_y.setRange(0, 100)
 
+        axis_x.setLabelsColor(Colors.TEXT_SECONDARY)
+        axis_y.setLabelsColor(Colors.TEXT_SECONDARY)
+
+        axis_x.setLabelsFont(
+            Typography.get_font(Typography.META, Typography.REGULAR)
+        )
+
+        axis_y.setLabelsFont(
+            Typography.get_font(Typography.META, Typography.REGULAR)
+        )
+
+        axis_x.setGridLineVisible(False)
+        axis_y.setGridLineColor(Qt.GlobalColor.lightGray)
+
         bar_chart.addAxis(axis_x, Qt.AlignmentFlag.AlignBottom)
         bar_chart.addAxis(axis_y, Qt.AlignmentFlag.AlignLeft)
 
@@ -113,7 +150,7 @@ class DashboardPresenter:
         bar_series.attachAxis(axis_y)
 
         bar_chart_view = QChartView(bar_chart)
-        bar_chart_view.setMinimumHeight(300)
+        bar_chart_view.setMinimumHeight(400)
 
         bar_chart_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
 
--- app/ui/views/pages/dashboard/stat_card.py
+++ app/ui/views/pages/dashboard/stat_card.py
@@ -1,6 +1,8 @@
-# app/ui//views/pages/dashboard/stat_card.py
+# app/ui/views/pages/dashboard/stat_card.py
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QFrame, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QFrame, QVBoxLayout
+
+from app.ui.views.components.sub_components.custom_texts import CustomDocumentTitle, CustomTitleMain
 
 
 class StatCard(QFrame):
@@ -9,35 +11,17 @@ class StatCard(QFrame):
         self._title = title
         self._value = value
 
-        self._setup_ui()
-
-        self.setStyleSheet("""
-            #stat_card {
-                padding: 15px;
-            }
-            
-            #stat_card_title_label {
-                font-size: 14px;
-            }
-            
-            #stat_card_value_label {
-                font-size: 22px; 
-                font-weight: bold;
-            }
-        """)
-
-    def _setup_ui(self) -> None:
-        self.setObjectName("stat_card")
+        self.setObjectName("CardStat")
         self.setFrameShape(QFrame.Shape.StyledPanel)
 
         layout = QVBoxLayout(self)
 
-        title_label = QLabel(self._title)
-        title_label.setObjectName("stat_card_title_label")
+        title_label = CustomDocumentTitle(self._title)
+        title_label.setObjectName("StatName")
         title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        value_label = QLabel(self._value)
-        value_label.setObjectName("stat_card_value_label")
+        value_label = CustomTitleMain(self._value)
+        value_label.setObjectName("StatValue")
         value_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         layout.addWidget(title_label)
